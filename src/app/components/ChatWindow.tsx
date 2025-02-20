6123f9f9069544f605ba41a449822d509df34f59
kingstech001 <mamahkingsleychukwuebuka@gmail.com>
2025-02-20 15:51:03 -0800

    we are good now

--- src/app/components/ChatWindow.tsx
+++ src/app/components/ChatWindow.tsx
@@ -7,7 +7,7 @@ import InputArea from './InputArea';
 
 export default function ChatWindow(props: ChatWindowProps) {
   return (
-    <div className="max-w-3xl mx-auto">
+    <div className="max-w-3xl mx-auto p-[10px]">
       {/* Chat output area */}
       <div className="p-4 mb-4 h-[70vh] overflow-y-auto py-[30px]">
         <OutputArea 
